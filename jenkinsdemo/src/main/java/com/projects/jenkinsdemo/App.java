e935b3f3d22e126f08328f36dd964882175aad88
Rushi <Rushi@DESKTOP-024J199.attlocal.net>
2018-12-16 23:33:29 -0600

    added test class

--- jenkinsdemo/src/main/java/com/projects/jenkinsdemo/App.java
+++ jenkinsdemo/src/main/java/com/projects/jenkinsdemo/App.java
@@ -4,10 +4,12 @@ package com.projects.jenkinsdemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World!");
+		}
+
+	}
 }
---
+++ jenkinsdemo/src/main/java/com/projects/jenkinsdemo/TestClass.java
@@ -0,0 +1,5 @@
+package com.projects.jenkinsdemo;
+
+public class TestClass {
+
+}
